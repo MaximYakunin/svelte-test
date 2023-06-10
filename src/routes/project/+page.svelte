79c663058fb69d0017f54743c92dd2b395d6e633
Maxim <yamaxdm18@mail.ru>
2023-06-10 19:51:36 +0300

    1

--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -1,10 +1,19 @@
 <svelte:head>
     <title>Портфолио</title>
 </svelte:head>
+<div class="container">
+    <h1>Мои статьи</h1>
 
-
+</div>
 <h1>Портфолио</h1>
 todo..
 <script>
     import { routes } from './routes.js';
 </script>
+<style>
+    .container{
+        margin: 50px auto;
+        max-width: 800px;
+        padding: 0 20px;
+        }
+</style>
