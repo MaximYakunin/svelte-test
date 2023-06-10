25e1a5fa95e350aaa843d3f4901e5eb6838d6778
Maxim <yamaxdm18@mail.ru>
2023-06-10 22:43:01 +0300

    1

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,3 +1,3 @@
 <div class="container">
-    <slot />
-    </div>
+    <slot/>
+</div>
--- src/routes/blog/posts/1/+page.svelte
+++ src/routes/blog/posts/1/+page.svelte
@@ -2,4 +2,4 @@
 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae esse quia rerum impedit ipsa, unde vitae sunt et quasi voluptatum
 nisi placeat deserunt veritatis corporis explicabo praesentium accusamus suscipit! Blanditiis.</p>
 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae esse quia rerum impedit ipsa, unde vitae sunt et quasi voluptatum
-    nisi placeat deserunt veritatis corporis explicabo praesentium accusamus suscipit! Blanditiis.</p>
+nisi placeat deserunt veritatis corporis explicabo praesentium accusamus suscipit! Blanditiis.</p>
