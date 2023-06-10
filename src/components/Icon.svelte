18e22d9c15f62ec194b3f364c0307893e40b4d55
Maxim <yamaxdm18@mail.ru>
2023-06-11 00:07:49 +0300

    1

---
+++ src/components/Icon.svelte
@@ -0,0 +1,13 @@
+<script>
+    const icons=[
+        {
+            name: 'check-mark',
+            path: '<path d="M19.77 4.9311.4 1.4L8.43 19.071-5.6-5.6 1.4-1.4 4.2 4.2L19.77 4.93m0-
+            2.83L8.43 13.441-4.2L0 13.4718.43 8.43L24 6.33 19.77 2.1z' fill="currentColor"></path>',
+         },
+         {
+           name:'delete',
+            
+         }
+    ]
+</script>
