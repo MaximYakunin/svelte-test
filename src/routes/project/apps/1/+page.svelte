6d4d75f97d209dacf970232f6be5bddcc86fd32b
Maxim <yamaxdm18@mail.ru>
2023-06-10 21:50:43 +0300

    1

--- src/routes/project/apps/1/+page.svelte
+++ src/routes/project/apps/1/+page.svelte
@@ -23,4 +23,31 @@
 </script>
 
 <style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        min-height: 100%;
+        padding: 5vmin;
+        box-sizing: border-box;
+        background: antiquewhite;
+    }
+    form {
+        width: 100%;
+        max-width: 500px;
+        display: flex;
+        align-items: center;
+        margin-bottom: 1rem;
+        }
+    input {
+        flex-grow: 1;
+        width: 0;
+        border: none;
+        border-bottom: 1px solid black;
+        background: transparent;
+        box-shadow: none;
+        font-size: 1.2rem;
+        margin: 0;
+        outline: none;
+    }
 </style>
