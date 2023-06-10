9bf39684ebecc03b970e6071f3c7dde2eabfdbec
Maxim <yamaxdm18@mail.ru>
2023-06-10 23:25:09 +0300

    1

--- src/routes/project/apps/1/+page.svelte
+++ src/routes/project/apps/1/+page.svelte
@@ -1,8 +1,15 @@
 <main>
+    <h1>My to-do list</h1>
     <form on:submit|preventDefault={add}>
     <input bind:value={newItem} placeholder="Enter to-do"/>
     <button class="add-todo" on:click={add}><span>+</span></button>
     </form>
+    <div class="todos">
+        {#each todoList as item, index}
+        <span class="todo_text">{item.task}</span>
+        <div class="todo_buttons"></div>
+         {/each}
+    </div>
 </main>
 <script>
     let newItem = "";
@@ -19,6 +26,7 @@
                             newItem = "";
                             }
                     }
+    import Icon from '../../../../components/Icon.svelte';
 
 </script>
 
