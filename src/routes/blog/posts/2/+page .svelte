1358560dc2798f0456b06c40c4c8eaef0b6313b4
Maxim <yamaxdm18@mail.ru>
2023-06-10 22:36:22 +0300

    1

--- src/routes/blog/posts/2/+page .svelte
+++ src/routes/blog/posts/2/+page .svelte
@@ -1,4 +1,4 @@
-<h1>My First Blog Post</h1>
+<h1>My Second Blog Post</h1>
 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae esse quia rerum impedit ipsa, unde vitae sunt et quasi voluptatum
 nisi placeat deserunt veritatis corporis explicabo praesentium accusamus suscipit! Blanditiis.</p>
 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae esse quia rerum impedit ipsa, unde vitae sunt et quasi voluptatum
