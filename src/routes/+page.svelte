edc7d0743d382f72dbf1dd3095f9d3d49bb8cf61
Maxim <yamaxdm18@mail.ru>
2023-06-10 18:00:16 +0300

    1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,8 @@
             <div class="container info-container">
                 <div class="info-text">
                     <h1> 
-                        <p class="animate-text-1">Lorem, ipsum dolor sit amet .</p>
-                        <p class="text-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. </p>
+                        <p class="animate-text-1">Lorem, ipsum dolor sit.</p>
+                        <p class="text-2">Lorem ipsum dolor sit amet. </p>
                     </h1>
                 </div>
                     <h2 class="subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus  amet perferendis excepturi.</h2>
