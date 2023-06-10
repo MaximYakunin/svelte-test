0e9a51b9d2c41a7b9484fb82dfb381e587de53d6
Maxim <yamaxdm18@mail.ru>
2023-06-10 21:20:12 +0300

    1

--- src/routes/project/apps/1/+page.svelte
+++ src/routes/project/apps/1/+page.svelte
@@ -1,6 +1,25 @@
 <script>
+    let newItem='';
+    let todoList=[];
+    function add() {
+        if (newItem !=="){
+            todoList=[
+                ..todoList,
+                {
+                    задача: newItem,
+                    завершено: false;
+                },
+            ];
+            newItem='';
+        }
+    }
+
 </script>
 <main>
+    <form on:submit|preventDefault={add}>
+    <input bind value={newItem} placeholder="Enter to-do"/>
+    <button class="add-todo" on:click={add}><span>+</span></button>
+    </form>
 </main>
 <style>
 </style>
