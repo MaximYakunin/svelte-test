c71c1ec852807695e28b1288af90020173294abe
Maxim <yamaxdm18@mail.ru>
2023-06-10 22:33:13 +0300

    1

---
+++ src/routes/blog/posts/2/+page copy.svelte
@@ -0,0 +1,5 @@
+<h1>My First Blog Post</h1>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae esse quia rerum impedit ipsa, unde vitae sunt et quasi voluptatum
+nisi placeat deserunt veritatis corporis explicabo praesentium accusamus suscipit! Blanditiis.</p>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae esse quia rerum impedit ipsa, unde vitae sunt et quasi voluptatum
+    nisi placeat deserunt veritatis corporis explicabo praesentium accusamus suscipit! Blanditiis.</p>
