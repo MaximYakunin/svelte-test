3acae477b20b4ee69c954468bbdd7f06fbda8195
Maxim <yamaxdm18@mail.ru>
2023-06-10 20:27:49 +0300

    1

--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -5,16 +5,19 @@
     <h1>Мои проекты</h1>
     <div class="apps">
         {#each routes as page} 
-        <div class="app"> 
-        <h2>{page.title}</h2>
-        <p>{page.body}</p>
-        </div> 
+            <div class="app"> 
+                <h2>{page.title}</h2>
+                <p>{page.body}</p>
+                <p class="readmore">
+                   <a class="link" href={`/project/apps/${page.id}`}>
+                    Читать далее
+                   </a>
+                </p>
+            </div> 
         {/each}
     </div>
 </div>   
 
-<h1>Портфолио</h1>
-todo..
 <script>
     import { routes } from './routes.js';
 </script>
@@ -23,5 +26,18 @@ todo..
         margin: 50px auto;
         max-width: 800px;
         padding: 0 20px;
+        }
+    .apps {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+        }
+    .app {
+        border: 1px solid #ddd;
+        padding: 10px;
+        box-shadow: 0 0 10px #eee;
+        }
+    .link {
+        color: rgb(10,10,139);
         }
 </style>
