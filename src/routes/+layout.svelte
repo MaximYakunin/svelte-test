93d4452687eace38013b3a85734baa41e832f2b4
Maxim <yamaxdm18@mail.ru>
2023-06-10 15:31:19 +0300

    1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,9 @@
     <div class="nav-container">
         <a href="/" class="nav-logo" title="Back to Homepage">Максим Якунин</a>
             <div class="nav-links">
-                {#each nav as link}
-                    <a href={link.href} class="link">{link.title}</a>
-                {/each}
+                {#if link.isShow}
+                   <a href={link.href} class="link">{link.title}</a>
+                {/if}
             </div>
     </div>
 </nav>
