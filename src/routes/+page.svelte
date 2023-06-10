53b5759bcb20a29977571f3888411f5d38b2b669
Maxim <yamaxdm18@mail.ru>
2023-06-10 16:59:32 +0300

    1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,8 +24,6 @@
 </div>
 <div class="background-div"></div>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <style>
     .info {
         text-align: center;
