4baca6e09df8cbd11b785383711f32e2e7a56b17
Maxim <yamaxdm18@mail.ru>
2023-06-11 17:07:57 +0300

    1

--- src/routes/project/apps/2/+page.svelte
+++ src/routes/project/apps/2/+page.svelte
@@ -1,29 +1,28 @@
 <h1>Учет расходов</h1>
-<main>
-    <form on:submit|preventDefault={add}>
-        <input bind:value={newItem} placeholder="Enter todo"/>
-        <button class="add-todo" on:click={add}><span>+</span></button>
-    </form>
-    <div class="todos">
-        {#each todoList as item, index}
-            <div class="todo" class:completed={item.completed}>
-                <span class="todo__text">{item.task}</span>
-                <div class="todo__buttons">
-                    <button class="complete" on:click={() => complete(index)}>
-                        <Icon name="check-mark" />
-                    </button>
-                    <button class="delete" on:click={() => remove(index)}>
-                        <Icon name="delete" />
-                </button>
-                </div>
-            </div>
-         {/each}
-    </div>
-</main>
+<table>
+<thead>
+        <tr>
+            <th>Название</th>
+            <th>Сумма</th>
+            <th>Действия</th>
+        </tr>
+</thead>
+<tbody>
+        {#each expenses as expense, i}
+    <tr>
+        <td><div>{expense.name}</div></td>
+        <td><div>{expense.amount}</div></td>
+        <td><button on:click={() => deleteExpense(i)}>Улалить</button></td>
+    </tr>
+        {/each}
+</tbody>
+</table>
 <script>
-import Icon from '../../../../components/Icon.svelte';    
-let newItem = '';
-let todoList = [];
+let expenses = [];
+let newName = '';
+let newAmount = '';
+
+
 function add() {
     if (newItem !== '') {
         todoList = [
@@ -36,96 +35,59 @@ function add() {
         newItem = '';
     }
 }
-function remove(index) {
-    todoList.splice(index, 1);
-    todoList = todoList;
-         }
-function complete(index) {
-    todoList[index].completed = !todoList[index].completed;
-        } 
+function addExpense() {
+    const obj = {
+name: newName,
+amount: parseInt(newAmount),
+};
+expenses.push(obj);
+expenses = expenses;
+newName = '';
+newAmount = '';
+}
+
+function deleteExpense(index) {
+    expenses.splice(index, 1);
+    expenses = expenses;
+}
+
 </script>
 <style>
-main {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        min-height: 100%;
-        padding: 5vmin;
-        box-sizing: border-box;
-        background: antiquewhite;
-    }
-form {
-        width: 100%;
-        max-width: 500px;
-        display: flex;
-        align-items: center;
-        margin-bottom: 1rem;
-        }
-input {
-        flex-grow: 1;
-        width: 0;
-        border: none;
-        border-bottom: 1px solid black;
-        background: transparent;
-        box-shadow: none;
-        font-size: 1.2rem;
-        margin: 0;
-        outline: none;
-    }
-.todos {
+label {
+    display: block;
+    margin-bottom: 10px;
+}
+input[type='text'] {
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin-right: 10px;
+    width: 150px;
+}
+table {
+    margin-top: 20px;
+    border-collapse: collapse;
     width: 100%;
-    max-width: 500px;
-    }
-.todo {
-    display: flex;
-    padding: 20px;
-    border-radius: 20px;
-    box-shadow: 0 0 15px rgb(0, 0, 0 /20%);
-    background-color: hsla(0, 0%, 100%, 0.2);
-    margin-top: 1rem;
-    font-size: 1.2rem;
-    justify-content: space-between;
-    align-items: center;
-    }
-.todo__buttons {
-        display: flex;
-        align-items: center;
-        margin-left: 1rem;
-    }
-.todo button {
-        width: 32px;
-        height: 32px;
-        padding: 4px;
-        margin: 0;
-        flex-shrink: 0;
-    }
-h1 {
-        text-align: center;
-        font-size: 1.5 rem;
-        margin: 2em 0;
-    }
+}
+th,
+td {
+    border: 1px solid #ccc;
+    padding: 8px;
+    text-align: left;
+}
+th {
+background-color: #eee;
+}
 button {
-        background-color: transparent;
-        border: none;
-    }
-button.delete,
-button.delete:hover {
-        color: brown;
-        transition: color 100ms ease-out;
-    }
-button.complete,
-button.complete:hover {
-        color: cadetblue;
-        transition: color 100ms ease-out;
-    }
-.todo.completed {
-        color: slategray;
-    }
-.todo.completed .todo__text {
-        text-decoration: line-through;
-    }
-    .todo.completed button {
-        color: silver;
-    }
-    
+    padding: 5px 10px;
+    background-color: #4caf50;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+h1 {
+        font-size: 24px;
+        margin-bottom: 20px;
+}   
 </style>
